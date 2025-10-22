69e4c0b08dd278af09b08e953124ad4a05d4c959
Joshua Soto <joshuasotomayor76@gmail.com>
2025-10-21 22:51:13 -0600

    primera prueba

---
+++ Ejercicio_CSV.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+NOMBRE_ARCHIVO = "Herramientas_disponibles_Taller_Chepetronica(Inventario).csv"
+
+def extrator_csv() -> list:
+    global NOMBRE_ARCHIVO
+    with open(NOMBRE_ARCHIVO, "r") as archivo:
+        contenido = archivo.readlines()
+        # print(contenido)
+        
+        lista_datos = []
+        for linea in contenido:
+            lista_datos.append( linea.strip().split(";") )
+
+    return lista_datos    
+
+def main():
+    lista_datos = extrator_csv()
+    tabla_datos = pd.DataFrame(lista_datos[1:], columns=lista_datos[0])
+
+    print(tabla_datos)
+
+
+if  __name__ == "__main__":
+    main()
